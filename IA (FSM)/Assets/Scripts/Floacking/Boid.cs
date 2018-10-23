8a0a224209a964b4418ce2706767ef7b303e64cd
Zerhe <dante_ro@hotmail.com.ar>
2018-10-22 21:44:32 -0300

    MouseInput

---
+++ IA (FSM)/Assets/Scripts/Floacking/Boid.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boid : MonoBehaviour {
+
+    List<Boid> visiblesBoids;
+    Vector3 directionMov;
+    Vector3 destinyPosition;
+    [SerializeField]
+    int velMov;
+    bool mov = false;
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if (Input.GetMouseButtonDown(0))
+        {
+            destinyPosition = MouseInput.MousePosition();
+            destinyPosition.y = transform.position.y;
+            directionMov = Direction.CalculateDirection(destinyPosition, transform.position);
+            mov = true;
+        }
+        if (mov)
+            transform.Translate(directionMov * Time.deltaTime * velMov);
+        if (Direction.CalculateDistance(destinyPosition, transform.position) < 1)
+            mov = false;
+
+    }
+}
---
+++ IA (FSM)/Assets/Scripts/Floacking/MouseInput.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class MouseInput 
+{
+    public static RaycastHit hit;
+    public static float maxDistanceRay = 100;
+
+    public static Vector3 MousePosition()
+    {
+        Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+        if (Physics.Raycast(ray, out hit, Camera.main.transform.position.y + maxDistanceRay))
+        {
+            return hit.point;
+        }
+        return Vector3.zero;
+    }
+}
